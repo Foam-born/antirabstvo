c87a8b0d301d657ba82a2449f6d2d328cb7e37d3
Эмма Мурашкина <saremmi@gmail.com>
2019-03-18 23:31:29 +0200

    Starting project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
+'use strict';
 
-Vue.config.productionTip = false
+// vendor
+import Vue from 'vue/dist/vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'slick-carousel/slick/slick.css'
+import AOS from 'aos/dist/aos.js'
+import 'aos/dist/aos.css'
+
+AOS.init({
+  disable: function () {
+    return window.innerWidth < 576;
+  }
+});
+
+// common
+import './common/styles/fonts.styl'
+import './common/styles/global-variables.styl'
+import './common/styles/style.styl'
+import './common/styles/mixins.styl'
+
+// bem components
+import './bem-components/logo/logo.styl'
+
+import './bem-components/site-header/site-header.styl'
+
+import './bem-components/header-nav/header-nav.styl'
+
+import './bem-components/section/section.styl'
+
+import './bem-components/services/services.styl'
+
+import './bem-components/sandwich/sandwich.styl'
+
+
+
+// vue components
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+
+  components: {
+
+  }
+});
